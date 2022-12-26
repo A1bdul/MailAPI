f810e19164960d7ebd976627e7787b490edb7b97
Abdulrahman Daromosu <73336016+A1bdul@users.noreply.github.com>
2022-12-26 04:49:04 +0100

    Update settings.py

--- djangoProject1/settings.py
+++ djangoProject1/settings.py
@@ -26,7 +26,7 @@ load_dotenv()
 SECRET_KEY = 'django-insecure-_#pdwwhgd0a5y#+-a%v__co9t3)kb8u(mcuo*!k_pzi8$g@uj5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
