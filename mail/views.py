be830ed1e8f5b12c107f01ef02f63d0036972253
Abdulrahman Daromosu <73336016+A1bdul@users.noreply.github.com>
2022-12-26 05:44:49 +0100

    Update views.py

--- mail/views.py
+++ mail/views.py
@@ -25,5 +25,6 @@ class MailAPIView(APIView):
         subject = request.data['name']
         msg = EmailMultiAlternatives(subject, message, settings.EMAIL_HOST_USER, ['a1daromosu@gmail.com', ])
         # msg.send()
+        print(message)
         EmailThread(msg).start()
         return Response(True)
