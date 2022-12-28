fe9ffd425331f110462d8a72f81ad26785ecb287
Abdulrahman Daromosu <73336016+A1bdul@users.noreply.github.com>
2022-12-28 01:29:55 +0100

    Update views.py

--- mail/views.py
+++ mail/views.py
@@ -27,25 +27,25 @@ class MailAPIView(APIView):
         message = request.data['message'] + '\nfrom ' + email
         subject = request.data['name']
         print(message)
-        configuration = sib_api_v3_sdk.Configuration()
-        configuration.api_key['api-key'] = str(os.getenv("SENDINBLUE_API_KEY"))
-        api_instance = sib_api_v3_sdk.AccountApi(sib_api_v3_sdk.ApiClient(configuration))
+        # configuration = sib_api_v3_sdk.Configuration()
+        # configuration.api_key['api-key'] = str(os.getenv("SENDINBLUE_API_KEY"))
+        # api_instance = sib_api_v3_sdk.AccountApi(sib_api_v3_sdk.ApiClient(configuration))
         # api_instance = sib_api_v3_sdk.TransactionalEmailsApi(sib_api_v3_sdk.ApiClient(configuration))
         # sender = {"name": "sender", "email": email}
         # to = [{"email": settings.EMAIL_HOST_USER, "name": "Abdul"}]
         # headers = {"Some-Custom-Name": "unique-id-1234"}
         # send_smtp_email = sib_api_v3_sdk.SendSmtpEmail(to=to, headers=headers,html_content=message, sender=sender, subject=subject)
-        try:
+        # try:
             # api_response = api_instance.send_transac_email(send_smtp_email)
-            api_response = api_instance.get_account()
-            pprint(api_response)
-            return Response(True)
-        except ApiException as e:
-            print("Exception when calling SMTPApi->send_transac_email: %s\n" % e)
-            return Response(False)
+            # api_response = api_instance.get_account()
+            # pprint(api_response)
+            # return Response(True)
+        # except ApiException as e:
+            # print("Exception when calling SMTPApi->send_transac_email: %s\n" % e)
+            # return Response(False)
         
-        # msg = EmailMultiAlternatives(subject, message, settings.EMAIL_HOST_USER, ['a1daromosu@gmail.com', ])
-        # msg.send()
-        # print(message)
-        # EmailThread(msg).start()
-        # return Response(True)
+        msg = EmailMultiAlternatives(subject, message, settings.EMAIL_HOST_USER, ['a1daromosu@gmail.com', ])
+        msg.send()
+        print(message)
+        EmailThread(msg).start()
+        return Response(True)
